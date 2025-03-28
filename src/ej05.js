60b09aacbe568f199e5732b48f8cbbda1a014cb2
Android Studio <devandroid@ort>
2025-03-28 10:22:50 -0300

    Ej05

--- src/ej05.js
+++ src/ej05.js
@@ -1,8 +1,13 @@
+import url from "url";
 let miUrl = null;
 let miObjeto = null;
 miUrl = 'http://www.ort.edu.ar:8080/alumnos/index.htm?curso=2022&mes=mayo';
 miObjeto = parsearUrl (miUrl);
-console.log(miObjeto);
-function parsearUrl(laURL){
-// No seas vago, acá hay que hacer el cuerpo de la función!!!
+console.log("Host: " + miObjeto.host);
+console.log("Pathname: " + miObjeto.pathname);
+console.log("Params: " + miObjeto.searchParams);
+
+function parsearUrl(miURL){
+    const laURL = new URL(miURL);    
+    return laURL
 }
--- src/modules/url.js
+++
@@ -1,8 +0,0 @@
-class URL{
-    URL(){
-        
-    }
-    parsaerUrl(url){
-        
-    }
-}
