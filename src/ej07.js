3c2f2ced78ef457307e5e4f2a83c3cf6ffef4def
AriJusid <48042387@est.ort.edu.ar>
2025-03-28 10:51:49 -0300

    Ej07

--- src/ej07.js
+++ src/ej07.js
@@ -1,10 +1,10 @@
-import { getCurrency, getCountryByAbbreviation } from 'currency-map-country';
+import { getCurrencyAbbreviation , getCountryByAbbreviation } from 'currency-map-country';
 let monedaDelPais, codigoPais;
 codigoPais = 'AR';
 monedaDelPais = obtenerMoneda(codigoPais);
 console.log(`La moneda del país ${codigoPais} es: ${monedaDelPais}`);
 
 function obtenerMoneda(code){
-   getCurrency(getCountryByAbbreviation(code))
+   return getCurrencyAbbreviation(getCountryByAbbreviation(code))
 
 }
