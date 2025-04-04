de9c7229f78280049746b524dc1f3ad478bd31e9
Android Studio <devandroid@ort>
2025-04-04 10:15:50 -0300

    Corrección Ej08

--- src/ej04.js
+++ src/ej04.js
@@ -3,6 +3,7 @@ const ARCHIVO_ENTRADA = "./archivo-entrada.txt";
 const ARCHIVO_SALIDA = "./archivo-salida.txt";
 console.clear();
 copiar(ARCHIVO_ENTRADA, ARCHIVO_SALIDA);
+
 function copiar(origen, destino) {
   fs.copyFile(origen, destino, (err) => {
     if (err) {
--- src/modules/omdb-wrapper.js
+++ src/modules/omdb-wrapper.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-
-const APIKEY          = "78296dac"; 
+const APIKEY = "78296dac"; 
+const RESULTADOSCONSOLA = 10
 
 
 const OMDBSearchByPage = async (searchText, page = 1) => {
@@ -17,13 +17,14 @@ const OMDBSearchByPage = async (searchText, page = 1) => {
     };
 
     try{
-    const requestString = `http://www.omdbapi.com/?apikey=${APIKEY}&s=cars`;
+    const requestString = `http://www.omdbapi.com/?apikey=${APIKEY}&s=cars&p=${page}`;
     const apiResponse = await axios.get(requestString);
 
     if(apiResponse.data.Response === "True"){
         returnObject.respuesta = true
         returnObject.cantidadTotal = apiResponse.data.totalResults
         returnObject.datos = apiResponse.data.Search
+
     }
 
     else{returnObject.respuesta = false}
@@ -49,23 +50,45 @@ const OMDBSearchComplete = async (searchText) => {
 
     };
 
-
+    
     try{
-        const requestString = `http://www.omdbapi.com/?apikey=${APIKEY}&s=${searchText}`;
-        const apiResponse = await axios.get(requestString);
-    
-        if(apiResponse.data.Response === "True"){
-            returnObject.respuesta = true
-            returnObject.cantidadTotal = apiResponse.data.totalResults
-            returnObject.datos = apiResponse.data.Search
+
+        let paginaReturn  = await OMDBSearchByPage(searchText, 1)
+
+        
+        var cantPages = Math.ceil(paginaReturn.cantidadTotal / RESULTADOSCONSOLA)
+
+        if(cantPages > 1){
+            returnObject = paginaReturn
+
+            for(var i = 2; i < cantPages; i++){
+                                        
+                  /*if(paginaReturn.data.Response === "True"){
+                    returnObject.respuesta = true
+                    returnObject.cantidadTotal = paginaReturn.data.totalResults
+                    returnObject.datos = paginaReturn.data.Search
+                }
+
+                else{returnObject.respuesta = false}
+                paginaReturn  = OMDBSearchByPage(searchText, i)
+                returnObject.datos = returnObject.datos.concat(paginaReturn.datos)*/
+
+                paginaReturn = await OMDBSearchByPage(searchText, 1)
+                returnObject.datos = returnObject.datos.concat(paginaReturn.datos)
+                                    
+            }
+
+            returnObject.cantidadTotal = returnObject.datos.length
+
+        }else{            
+            returnObject = paginaReturn
+
         }
-    
-        else{returnObject.respuesta = false}
-      
-        }
-        catch{
-            console.log("error")
-        }
+
+       
+        
+    }
+    catch(error){console.log(error)}
 
   return returnObject;
 
